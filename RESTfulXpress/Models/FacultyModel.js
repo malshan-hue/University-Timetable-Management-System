2a154cfc2e72d38c3dd420d0ea4dea9bddc910f8
Malshan Rathnayake <malshan.rathnayake@iits.biz>
2024-04-14 00:34:03 +0530

    separeted RESTfullExpress from same repo

--- RESTfulXpress/Models/FacultyModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const facultySchema = new mongoose.Schema({
-    FacultyId: String,
-    FacultyName: String,
-    IsDeleted: Boolean
-});
-
-module.exports = mongoose.model('FacultyModel', facultySchema);
--- RESTfulXpress/Routes/AuthRoute.js
+++
@@ -1,8 +0,0 @@
-const { SignUp, Login, Welcome } = require("../Controllers/AuthController");
-const router = require("express").Router();
-
-router.post('/signup', SignUp);
-router.post('/login', Login);
-router.get('/', Welcome);
-
-module.exports = router;
